c2e216c9bedda95a251092bf1e60eb58c43d9fc3
Amos Lee <iwillhappy1314@gmail.com>
2023-08-13 02:08:54 +0800

    Add plugin file for testing purpose

---
+++ wenprise-eloquent.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Wenprise Eloquent
+Plugin URI: https://www.wpzhiku.com/
+Description: Laravel Eloquent in WordPress
+Version:            1.0.0
+Author: 一刀
+Author URI: https://www.wpzhiku.com
+*/
+
+require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
